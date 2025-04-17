1dbf3e347b413d08ff5f3887afd2ce2145f0b1db
Ogtavin <joaosteam2021@gmail.com>
2025-04-17 00:39:37 +0000

    exercicio 1

---
+++ tv.js
@@ -0,0 +1,14 @@
+let dados =[97, 45, 49, 32, 2, 5, 9, 99, 1, 7,];
+
+// percorrer todos os itens do array
+for (let i = 0; i< dados.length; i++){
+    console.log("O indice e: " + 
+        i +
+         " o valor e: " + 
+         dados[i] );
+}
+//para cada item, verificar se ele e maior que o proximo
+
+//se for, inverter ambos os valores.
+
+//caso contrario,
